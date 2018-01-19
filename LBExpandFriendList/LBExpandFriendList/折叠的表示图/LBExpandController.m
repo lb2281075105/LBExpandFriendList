ddd1465c745d9ef84f3bcd777834569377ff8bcc
yunmei <yunmei@yunmeideiMac.lan>
2018-01-19 09:35:14 +0800

    初始化项目

---
+++ LBExpandFriendList/LBExpandFriendList/折叠的表示图/LBExpandController.m
@@ -0,0 +1,23 @@
+//
+//  LBExpandController.m
+//  LBExpandFriendList
+//
+//  Created by yunmei on 2018/1/19.
+//  Copyright © 2018年 yunmei. All rights reserved.
+//
+
+#import "LBExpandController.h"
+
+@interface LBExpandController ()
+
+@end
+
+@implementation LBExpandController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    self.view.backgroundColor = [UIColor redColor];
+}
+@end
